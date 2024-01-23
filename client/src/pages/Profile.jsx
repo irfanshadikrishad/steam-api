8ad552017b8aa4d0926eb3b713a5a5cc58246eb0
Irfan Shadik Rishad <irfanshadikrishad@gmail.com>
2024-01-24 04:46:19 +0600

    demo

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { IoPersonAddSharp } from "react-icons/io5";
 
 export default function Profile() {
     const { id } = useParams();
@@ -19,13 +20,35 @@ export default function Profile() {
             setUser(response);
         }
     }
-    console.log(user);
+
     useEffect(() => {
         getUser();
     }, [])
     return (
-        <section>
-            <div>{id}</div>
+        <section className="profile">
+            {<section className="profile_card">
+                <section className="profile_part1">
+                    <div className="image_container">
+                        <img className="profile_image" src={user.avatar} alt={user.username} draggable="false" />
+                        <img className="profile_frame" src={user.avatarFrame} alt={user.username} draggable="false" />
+                    </div>
+                    <div>
+                        <div className="profile_username">{user.username} {user.actualName && `(${user.actualName})`}</div>
+                        <div className="profile_level">Level: {user.level}</div>
+                        <p>{user.status} {user.status === "Currently In-Game" && `(${user.gameName})`}</p>
+                    </div>
+                </section>
+                <section className="profile_stats">
+                    {user.stats && user.stats.map((user, i) => {
+                        return <p key={i}>{user.title} {user.count}</p>
+                    })}
+                </section>
+                <a
+                    className="profile_add"
+                    href={`https://steamcommunity.com/id/${id}`}
+                    target="_blank">{<IoPersonAddSharp />}
+                </a>
+            </section>}
         </section>
     )
 }
